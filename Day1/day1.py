880f81e71ac0c211f6a3ff612e751f3dac241ca5
kmsalah <salah4501@gmail.com>
2017-12-02 14:39:02 -0500

    Day 1 Done, modulo was the key for the one

--- Day1/day1.py
+++ Day1/day1.py
@@ -8,7 +8,8 @@ numDigits = len(arrayOfDigits)
 '''
 PART 1
 ------------
-The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
+The captcha requires you to review a sequence of digits (your puzzle input) and find the sum of all digits 
+that match the next digit in the list. The list is circular, so the digit after the last digit is the first digit in the list.
 
 For example:
 
@@ -25,7 +26,9 @@ print("Part 1 Answer: " + str(sumPartOne))
 
 '''
 PART 2
-Now, instead of considering the next digit, it wants you to consider the digit halfway around the circular list. That is, if your list contains 10 items, only include a digit in your sum if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
+Now, instead of considering the next digit, it wants you to consider the digit halfway around 
+the circular list. That is, if your list contains 10 items, only include a digit in your sum 
+if the digit 10/2 = 5 steps forward matches it. Fortunately, your list has an even number of elements.
 
 For example:
 
